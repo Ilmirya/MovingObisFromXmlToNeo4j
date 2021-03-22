0a488e120257a6c62704e145577c64bed0129327
Ilmirya <yanturin.ilmir@gmail.com>
2021-03-22 22:52:04 +0500

    load app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MovingObisFromXmlToNeo4j
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
