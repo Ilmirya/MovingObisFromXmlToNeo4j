d71262a949ed60312ac4c1c0a4e6e57716141cb7
Yanturin Ilmir <yanturin.ilmir@gmail.com>
2021-04-07 22:59:39 +0500

    парсинг данных из xml

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace MovingObisFromXmlToNeo4j
 {
@@ -6,7 +7,52 @@ namespace MovingObisFromXmlToNeo4j
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XmlDocument xml = new XmlDocument();
+            Console.WriteLine("Write full path to file:");
+            string path = Console.ReadLine();//F:\projects\MovingObisFromXmlToNeo4j\128.gxc
+            //xml.Load(path);
+            xml.Load("F:\\projects\\MovingObisFromXmlToNeo4j\\128.gxc");
+            XmlElement xmlElement = xml.DocumentElement;
+            foreach(XmlNode node in xmlElement)
+            {
+                Child(node);
+            }
         }
+        private static void Child(XmlNode node)
+        {
+            if(node.Name == "GXDLMSObject")
+            {
+                DLMSObject(node);
+            }
+            else
+            {
+                foreach (XmlNode child in node.ChildNodes)
+                {
+                    Child(child);
+                }
+            }
+        }
+        private static void DLMSObject(XmlNode node)
+        {
+            string type = node.Attributes[NodeName.Type].Value.Substring(6);
+            string logicalName, description;
+            foreach (XmlNode child in node.ChildNodes)
+            {
+                if(child.Name == NodeName.LogicalName)
+                {
+                    logicalName = child.ChildNodes[0].Value;
+                }
+                else if (child.Name == NodeName.Description)
+                {
+                    description = child.ChildNodes[0].Value;
+                }
+            }
+        } 
+    }
+    class NodeName
+    {
+        public static string LogicalName = "LogicalName";
+        public static string Description = "Description";
+        public static string Type = "xsi:type";
     }
 }
